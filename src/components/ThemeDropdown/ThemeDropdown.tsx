64344b450323c2065f914fae64ca1420bdb1fda9
loach99 <constantinent@mail.ru>
2024-12-15 00:50:18 +0300

    add default theme

--- src/components/ThemeDropdown/ThemeDropdown.tsx
+++ src/components/ThemeDropdown/ThemeDropdown.tsx
@@ -2,13 +2,13 @@ import { themeList } from '../../constants/codeLanguage';
 import styles from './styles/Select.module.css'
 interface ThemeProps {
     handleThemeChange: (name: string) => void,
-    selectedTheme: string
+    selectedTheme: string | null
     
 }
 const ThemeDropdown = ({ handleThemeChange,selectedTheme }: ThemeProps) => {
     return (
         <div>
-            <select className={styles.select} defaultValue={selectedTheme} onChange={(e) => handleThemeChange(e.target.value)}>
+            <select className={styles.select} defaultValue={selectedTheme || 'vs'} onChange={(e) => handleThemeChange(e.target.value)}>
               {themeList?.map((option) => (
                 <option key={option.id}>{option.value}</option>
               ))}
